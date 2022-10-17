c58a575e5f1c3b3f236b7a9eeb647c5291da1041
SpaceTheFinalFrontier1966 <porihuela1683@gmail.com>
2022-10-17 14:21:52 -0700

    Commit 1
    "Basic set up of website. Labs 1-3 completed."

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Orihuela, Philip
+ * Created:   10.17.2022
+ * License: Orihuela Corp.
+ */
+
+ // Define Variables
+ make = "Ghia";
+ model = "Volkswagen Karmann";
+ color = "light blue";
+ year = 1968;
+
+// calculate
+age = 2022 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
