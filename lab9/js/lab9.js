07e2308a25d844054a2a366e1f8cc97a9d828394
SpaceTheFinalFrontier1966 <porihuela1683@gmail.com>
2022-10-31 11:31:14 -0700

    Lab 9 stuff

---
+++ lab9/js/lab9.js
@@ -0,0 +1,19 @@
+/**
+*Author: Wei-Yan Chiang, Philip Orihuela
+*Created: October 30, 2022
+*License: Public Domain
+*/
+
+var outputEL = getElementById("output");
+
+var new1El = document.createELement("p");
+var new2El = document.createElement("p");
+
+new1El.innerHTML="I'm Batman!";
+new2El.innerHTML="I don't know half of you half as well as I should like, and I like less than half of you half as well as you deserve.";
+
+outputEL.appendChild(new1El);
+outputEl.appendChild()
+
+new1El.style..fontFamily="Helvetica";
+new2El.style.fontSize="40px";
