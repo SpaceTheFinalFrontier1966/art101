f6b15dc860e036151c4f1664ac000331bf72038f
SpaceTheFinalFrontier1966 <porihuela1683@gmail.com>
2022-11-16 21:07:46 -0800

    stuff again

--- lab13/js/lab11.js
+++ lab13/js/lab11.js
@@ -1,9 +1,11 @@
 /**
-*Author: DWei-Yan Chiang, Philip Orihuela
+*Author: Wei-Yan Chiang, Philip Orihuela
 *Created: November 14, 2022
 *License: Public Domain
 */
 
+//did you and jocelyn do this together? I believe I did this myself with minor assitance from her. I see.
+//can you put your github username so I can see how this page currently runs? "SpaceTheFinalFrontier1966" I forget why I did it Star Trek themed.
 fucntion fizzBuzzBoom(maxNums,factorObj) {
   for (var num=0; num<maxNums; num++) {
     var outputStr = "";
@@ -12,7 +14,7 @@ fucntion fizzBuzzBoom(maxNums,factorObj) {
         outputStr += factorObj[factor];
       }
     }
-    if (outputStr) {
+    if (outputStr != 0) {
       outputStr = " - " + outputStr + "!";
     }
     outputToPage(num.toString() + outputStr)
